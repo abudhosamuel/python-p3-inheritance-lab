6c16ced2fc888193fe90c54e74c55e476444d48f
abudhosamuel <abudhosamuel@gmail.com>
2024-09-02 22:11:09 +0300

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,14 @@
-#!/usr/bin/env python
+# Ensure that the User class is imported correctly
+from lib.user import User
 
-from user import User
-
+# Define the Student class as a subclass of User
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        # Initialize the parent class (User)
+        super().__init__(first_name, last_name)
+        # Initialize the knowledge attribute as an empty list
+        self.knowledge = []
+
+    def learn(self, knowledge_str):
+        # Add knowledge to the student's knowledge list
+        self.knowledge.append(knowledge_str)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,25 @@
-#!/usr/bin/env python
-
-from user import User
+# Ensure that the User class is imported correctly
+from lib.user import User
 
 import random
 
+# Define the Teacher class as a subclass of User
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        # Initialize the parent class (User)
+        super().__init__(first_name, last_name)
+        # Initialize the knowledge attribute with predefined knowledge
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        # Return a random piece of knowledge
+        return random.choice(self.knowledge)
